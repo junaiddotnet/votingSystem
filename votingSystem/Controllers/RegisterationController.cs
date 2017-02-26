97b293ccd6108c7f142c45574e55e67bef6f3ba0
junaid <junaid@DESKTOP-TS8N4AP>
2017-02-26 02:55:58 +0000

    a

---
+++ votingSystem/Controllers/RegisterationController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace votingSystem.Controllers
+{
+    public class RegisterationController : Controller
+    {
+        // GET: Registeration
+        public ActionResult Index()
+        {
+            ControllerContext.HttpContext.Session["name"] = "junaid Mahmood";
+            
+            return View();
+        }
+        public ActionResult show ()
+        {
+            string name = ControllerContext.HttpContext.Session["name"].ToString();
+            return View("show",name);
+        }
+    }
+}
---
+++ votingSystem/Infastructure/SessionStateHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace votingSystem.Infastructure
+{
+    public static class SessionStateHelper
+    {
+        public enum SessionStateKeys
+        {
+            NAME
+        }
+        public static object Get(SessionStateKeys key)
+        {
+            string keyString = Enum.GetName(typeof(SessionStateKeys), key);
+            return HttpContext.Current.Session[keyString];
+        }
+        public static object Set(SessionStateKeys key,object Value)
+        {
+            string keyString = Enum.GetName(typeof(SessionStateKeys), key);
+            return HttpContext.Current.Session[keyString]=Value;
+        }
+    }
+}
